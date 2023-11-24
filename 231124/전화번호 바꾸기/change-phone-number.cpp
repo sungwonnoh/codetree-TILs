ed4301a0f65eaa1ebbd5013818ba830d77e5fe8a
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-11-24 15:08:42 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231124/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,frontn, backn;
+    cin >>n;
+    cin>>frontn;
+    cin.get();
+    cin>>backn;
+    cin.get();
+    cout<<"010-"<<backn<<frontn;
+    return 0;
+}
