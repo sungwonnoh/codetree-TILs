97518fc3970a9bbaca23603afc512684a6cf35f1
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-11-28 16:22:39 +0900

    Title: 별표 출력하기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231128/별표 출력하기 4/print-star-4.cpp
+++ 231128/별표 출력하기 4/print-star-4.cpp
@@ -5,12 +5,12 @@ int main() {
     int n;
     cin>>n;
     for(int i=0;i<n;i++){
-        for(int j=n-i;j>0;j--){
+        for(int j=0;j<n-i;j++){
             cout<<"* ";
         }cout<<endl;
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=2;j<n-1+i;j++){
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=i;j++){
             cout<<"* ";
         }cout<<endl;
     }
