fc1cf3c52730b1494f182eaa648ceed2917aeeb6
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-12-01 16:21:52 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231201/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    while(true){
+        cin >> num;
+        if(num==0){
+            break;
+        }
+        cout<<num<<endl;
+    }
+    return 0;
+}
