901826ee634461de35e5f641a5adbd70bad60e32
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-12-27 01:07:40 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m;
+    cin >>n >>m;
+    int arr1[n][m];
+    int arr2[n][m];
+    int newarr[n][m]={};
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr1[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr2[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(arr1[i][j] != arr2[i][j]){
+                newarr[i][j]=1;
+            }else{
+                newarr[i][j]=0;
+            }
+            cout<<newarr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
