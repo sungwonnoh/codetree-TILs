81cd111e7fa7831828aea247c48ad9d134225c8d
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-12-27 11:23:54 +0900

    Title: 지그재그로 숫자 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231227/지그재그로 숫자 채우기/zigzag-numbering.cpp
+++ 231227/지그재그로 숫자 채우기/zigzag-numbering.cpp
@@ -6,36 +6,23 @@ int main() {
     cin>>n>>m;
     int arr[n][m];
     int a=0;
-    /*
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            arr[i][j] = a;
-            a+=1;
-        }
-    }
-    for (int j = 0; j < n; j++) {
-        for (int i = 0; i < m; i++) {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
-    }*/
 
     
-    for(int i=0;i<n;i++){
+    for(int i=0;i<m;i++){
         if(i%2==0){//짝수 번째
-            for(int j=0;j<m;j++){
-                arr[i][j] = a;
+            for(int j=0;j<n;j++){
+                arr[j][i] = a;
                 a+=1;
             }
         }else{
-            for(int j=m-1;j>=0;j--){
-                arr[i][j] = a;
+            for(int j=n-1;j>=0;j--){
+                arr[j][i] = a;
                 a+=1;
             }
         }
     }
-    for (int j = 0; j < n; j++) {
-        for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
