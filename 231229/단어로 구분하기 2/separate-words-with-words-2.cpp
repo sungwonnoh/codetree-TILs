688734e8867d67ecbfc503059712979eedf20240
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-12-29 00:46:04 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[10];
+    for(int i=0;i<10;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<10;i++){
+        if(i%2==0){
+            cout<<arr[i]<<endl;
+        }
+    }
+    return 0;
+}
