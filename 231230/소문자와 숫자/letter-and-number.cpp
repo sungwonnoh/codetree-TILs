84192e7ffd308646558d988329d1a35df15ad349
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2023-12-30 01:56:17 +0900

    Title: 소문자와 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231230/소문자와 숫자/letter-and-number.cpp
+++ 231230/소문자와 숫자/letter-and-number.cpp
@@ -11,7 +11,7 @@ int main() {
         }
     }
     for(int i=0; i<s.length(); i++){
-        if((s[i]>='a'&&s[i]<='z')||(s[i]>='1'&&s[i]<='9')){
+        if((s[i]>='a'&&s[i]<='z')||(s[i]>='0'&&s[i]<='9')){
             cout<<s[i];
         }
     }
