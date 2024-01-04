2cfee978bbedfef497b9c953cfe84d566aebaeb5
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-04 12:19:14 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240104/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240104/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 using namespace std;
-int gcd(int a, int b){
-    int c;
-    while(b!=0){
-        c=a%b;
-        a=b;
-        b=c;
-    }
-    return a;
-}
 
+int gcd(int a, int b){
+    if(a==0){
+        return b;
+    }
+    return gcd(b%a,a);
+}
 int lcm(int a,int b){
     return (a*b) / gcd(a,b);
 }
