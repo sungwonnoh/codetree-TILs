669a63d0cc7c4d686ef02e5df7e383c11841ac86
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-07 15:18:36 +0900

    Title: 단어 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240107/단어 정렬/sorting-words.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string str[100];
+    for(int i=0;i<n;i++){
+        cin>>str[i];
+    }
+    sort(str, str+n);
+    for(int i=0;i<n;i++){
+        cout<<str[i]<<endl;
+    }
+    return 0;
+}
