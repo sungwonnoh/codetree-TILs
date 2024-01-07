91092147dc4c901a3a7344975e43c6cb2d219dcb
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-07 15:24:50 +0900

    Title: Top K 숫자 구하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240107/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    } 
+    sort(arr, arr+n);
+    cout<<arr[k-1];
+    return 0;
+}
