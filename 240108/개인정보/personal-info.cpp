6f246e76749d854cdce1875e28a689f88bc67eb3
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-08 22:05:49 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/개인정보/personal-info.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Profile{
+    public:
+        string name;
+        int h;
+        double w;
+        Profile(string name, int h, double w){
+            this->name=name;
+            this->h=h;
+            this->w=w;
+        }
+        Profile(){}
+};
+bool n(Profile a, Profile b){
+    return a.name<b.name;
+}
+bool height(Profile a, Profile b){
+    return a.h>b.h;
+}
+int main() {
+    Profile pro[5];
+    for(int i=0;i<5;i++){
+        string n;
+        int h;
+        double w;
+        cin>>n>>h>>w;
+        pro[i]=Profile(n,h,w);
+    }
+    sort(pro, pro+5, n);
+    cout<<"name"<<endl;
+    for(int i=0;i<5;i++){
+        cout<<pro[i].name<<" "<<pro[i].h<<" "<<pro[i].w<<endl;
+    }
+    cout<<endl;
+    sort(pro,pro+5,height);
+    cout<<"height"<<endl;
+    for(int i=0;i<5;i++){
+        cout<<pro[i].name<<" "<<pro[i].h<<" "<<pro[i].w<<endl;
+    }
+    return 0;
+}
