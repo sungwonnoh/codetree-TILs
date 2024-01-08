aa257fb6f73047c5eb6c5bbe4dfe6b5f94039403
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-08 23:02:51 +0900

    Title: 줄 세우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240108/줄 세우기 2/line-up-students-2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Line{
+    public:
+        int h,w,idx;
+        Line(int h, int w, int idx){
+            this->h=h;
+            this->w=w;
+            this->idx=idx;
+        }
+        Line(){}
+};
+bool cmp(Line a, Line b){
+    if(a.h==b.h){
+        return a.w<b.w;
+    }
+    return a.h<b.h;
+}
+int main() {
+    int n;
+    cin>>n;
+    Line line[1000];
+    for(int i=0;i<n;i++){
+        int h,w,idx;
+        cin>>h>>w;
+        idx = i;
+        line[i]=Line(h,w,idx+1);
+    }
+    sort(line, line+n, cmp);
+    for(int i=0;i<n;i++){
+        cout<<line[i].h<<" "<<line[i].w<<" "<<line[i].idx<<endl;
+    }
+    return 0;
+}
