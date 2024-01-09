eec213685254b7ef7c5102b771315b503bfaf4d6
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-09 15:50:44 +0900

    Title: k번째로 신기한 문자열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240109/k번째로 신기한 문자열/kth-special-string.cpp
+++ 240109/k번째로 신기한 문자열/kth-special-string.cpp
@@ -7,26 +7,21 @@ int main() {
     string t;
     cin>>n>>k>>t;
 
-    string arr[100];
+    string arr[100]={};
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int cnt = 0;
+    sort(arr,arr+n);
+    int cnt =0;
     for(int i=0;i<n;i++){
         if(arr[i].compare(0,t.length(),t)==0){
             cnt++;
-        }
-    }
-    int kpoint =0;
-    for(int i=0;i<n;i++){
-        if(arr[i].compare(0,t.length(),t)==0){
             if(cnt == k){
-                kpoint = i;
+                cout<<arr[i];
                 break;
             }
-            cnt++;
         }
     }
-    cout<<arr[kpoint];
+    
     return 0;
 }
