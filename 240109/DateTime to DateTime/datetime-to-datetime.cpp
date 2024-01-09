7c1cc309c9ae8b6942d8d756a79efded418fbfda
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-09 21:44:53 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/DateTime to DateTime/datetime-to-datetime.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    int datetime=0;
+    cin>>a>>b>>c;
+    if(a<11){
+        cout<<"-1";
+    }else if(a==11&&b<11){
+        cout<<"-1";
+    }else if(a==11&&b==11&&c<11){
+        cout<<"-1";
+    }else{
+        datetime = (a-11)*1440 + (b-11)*60 + c-11;
+    }
+
+    cout<<datetime;
+    return 0;
+}
