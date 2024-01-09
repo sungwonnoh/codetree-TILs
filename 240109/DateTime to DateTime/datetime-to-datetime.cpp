b739d42fe6e9d0c96365cc44e1de5e9021fa1cc9
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-09 22:04:29 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240109/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240109/DateTime to DateTime/datetime-to-datetime.cpp
@@ -5,16 +5,12 @@ int main() {
     int a,b,c;
     int datetime=0;
     cin>>a>>b>>c;
-    if(a<11){
-        cout<<"-1";
-    }else if(a==11&&b<11){
-        cout<<"-1";
-    }else if(a==11&&b==11&&c<11){
+    if(a<11||(a==11&&b<11)||(a==11&&b==11&&c<11)){
         cout<<"-1";
     }else{
         datetime = (a-11)*1440 + (b-11)*60 + c-11;
+        cout<<datetime;
     }
-
-    cout<<datetime;
+    
     return 0;
 }
