7bd06bedd8e96a798cf3ef3bc857c02581c604bb
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-11 01:22:50 +0900

    Title: 최대로 겹치는 지점, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240111/최대로 겹치는 지점/maximum-overlapped-points.cpp
+++ 240111/최대로 겹치는 지점/maximum-overlapped-points.cpp
@@ -9,15 +9,14 @@ int main() {
     int x1[100],x2[100];
     for(int i=0;i<n;i++){
         cin>>x1[i]>>x2[i];
-        arr[i]++;
     }
     for(int i=0;i<n;i++){
-        for(int j=x1[i];j<x2[i];j++){
+        for(int j=x1[i];j<=x2[i];j++){
             arr[j]++;
         }
     }
     int maxnum = arr[0];
-    for(int i=0;i<n;i++){
+    for(int i=0;i<100;i++){
         maxnum = max(maxnum, arr[i]);
     }
     cout<<maxnum;
