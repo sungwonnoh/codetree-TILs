1880f35c7632544439c2a91962fb3e94b35d12ed
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-11 00:02:07 +0900

    Title: 여러가지 진수변환, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/여러가지 진수변환/various-numeral-system-transformations.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,b;
+    int change[1000]={};
+    int cnt =0;
+    cin>>n>>b;
+    
+    while(true){
+        if(n<4){
+            change[cnt++]=n;
+            break;
+        }else{
+            change[cnt++] = n%b;
+            n/=b;
+        }
+    }
+    for(int i=cnt-1;i>=0;i--){
+        cout<<change[i];
+    }
+    return 0;
+}
