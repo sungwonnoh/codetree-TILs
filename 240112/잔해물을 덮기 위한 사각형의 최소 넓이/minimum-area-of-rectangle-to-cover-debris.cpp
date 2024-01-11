28092da9471e3eb423772a3cc2f8448acbea52a8
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-12 02:02:21 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 17ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240112/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 240112/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -13,12 +13,22 @@ int main() {
         for(int j=x1;j<x2;j++){
             for(int k=y1;k<y2;k++){
                 arr[j][k]+=1;
+            }
+        }
+    }
+
+    for(int i=0;i<2;i++){
+        for(int j=x1;j<x2;j++){
+            for(int k=y1;k<y2;k++){
                 if(i==1&&arr[j][k]==1){
+                    arr[j][k]=0;
+                }else if(i==0&&arr[j][k]==2){
                     arr[j][k]=0;
                 }
             }
         }
     }
+
     int cnt=0;
     for(int i=0;i<2001;i++){
         for(int j=0;j<2001;j++){
