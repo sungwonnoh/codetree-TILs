af7982091f3cd3e4f875e8992fe18c3432ed42fe
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-17 23:31:12 +0900

    Title: 문자에 따른 명령 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240117/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240117/문자에 따른 명령 2/text-based-commands2.cpp
@@ -10,29 +10,9 @@ int main() {
     for(int i=0;i<s.length();i++){
         if(s[i]=='L'){
             dir=(dir-1+4)%4;
-            /*
-            if(dir==0){
-                dir=3;
-            }else if(dir==1){
-                dir=0;
-            }else if(dir==2){
-                dir=1;
-            }else{
-                dir=2;
-            }*/
         }else if(s[i]=='R'){
-            /*
-            if(dir==0){
-                dir=1;
-            }else if(dir==1){
-                dir=2;
-            }else if(dir==2){
-                dir=3;
-            }else{
-                dir=0;
-            }*/
-            dir=(dir-1)%4;
-        }else{
+            dir=(dir+1)%4;
+        }else if(s[i]=='F'){
             x+=dx[dir], y+=dy[dir];
         }
     }
