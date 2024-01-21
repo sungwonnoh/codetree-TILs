eca00b43f5a753f6ec1dd9118cf5a72b175b3620
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-22 01:21:24 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n,m,k;
+    int idx=-1;
+    cin>>n>>m>>k;
+    vector<int> s(n+1);
+    for(int i=0;i<m;i++){
+        int num;
+        cin>>num;
+        s[num]++;
+    }
+    for(int i=0;i<n;i++){
+        if(s[i]>=k){
+            idx=i;
+            break;
+        }
+    }
+    cout<<idx;
+    return 0;
+}
