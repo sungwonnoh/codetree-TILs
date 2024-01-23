25e3b1bb66ab0ce52ebe30314d3ef83e06d5c287
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-23 16:54:48 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240123/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240123/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 using namespace std;
-bool inRange(int x, int y, int n, int m){
-    return x>=0&&x<n && y>=0&&y<m;
-}
+
 int main() {
     int n,m;
     cin>>n>>m;
+    int arr[100][100]={};
+    auto inRange = [&](int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < m;
+    };
+
     int dx[4]={0,1,0,-1};
     int dy[4]={1,0,-1,0};
-    int arr[100][100];
     int x=0,y=0, now=0;
     arr[x][y]=1;
+    
     for(int i=2;i<=n*m;i++){
         int nx= x+dx[now];
         int ny= y+dy[now];
-        if(arr[nx][ny]!=0 || !inRange(nx, ny,n,m)){
+        if(arr[nx][ny]!=0 || !inRange(nx, ny)){
             now = (now+1) %4;
         }
         x+=dx[now];
