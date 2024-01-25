c2290039d57b960cfa32a12e745210b2f1a1c827
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-25 14:22:12 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240125/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 using namespace std;
+
 int n;    
-bool inRange(int x, int y){
-    return x>=0&&x<n && y>=0&&y<n;
+bool inRange(int x, int y) {
+    return x >= 0 && x < n && y >= 0 && y < n;
 }
 
 int main() {
-    cin>>n;
-    int x=n/2,y=n/2,now=0;
-    int arr[100][100]={};
-    int dx[4]={0,1,0,-1};
-    int dy[4]={1,0,-1,0};
-    arr[x][y]=1;
+    cin >> n;
+    int arr[100][100] = {};
+    int dx[4] = {0, -1, 0, 1};
+    int dy[4] = {1, 0, -1, 0};
 
-    for(int i=1;i<=n*n;i++){
-        arr[x][y]=i;
-        int nx = x + dx[now];
-        int ny = y + dy[now];
-        if (!inRange(nx, ny) || arr[nx][ny] != 0) {
-            now = (now -1+4) % 4;  
+    int x = n/2, y = n/2;
+    int now = 0; 
+    int cnt = 1; //이동할 횟수
+    int num = 1; 
+
+    while (num <= n * n) {
+        for (int i = 0; i < cnt; i++) {
+            if (inRange(x, y)) {
+                arr[x][y] = num;
+                num++;
+            }
+            x += dx[now];
+            y += dy[now];
         }
-        x+=dx[now];
-        y+=dy[now];
-            
-        
+        now = (now + 1) % 4;
+        if (now % 2 == 0) {
+            cnt++;
+        }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<arr[i][j]<<" ";
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     return 0;
