a7b3c53f4e46b8b93363210b699a08a1dd311da0
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-25 11:52:33 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240125/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int n,m;
+bool inRange(int x, int y) {
+    return 0 <= x && x < n && 0 <= y && y < m;
+};
+int main() {
+    cin>>n>>m;
+    int arr[100][100]={};
+    int dx[4]={0,1,0,-1};
+    int dy[4]={-1,0,1,0};
+    int x=0,y=0, now=0;
+    arr[x][y]=1;
+    
+    for(int i=2;i<=n*m;i++){
+        int nx= x+dx[now];
+        int ny= y+dy[now];
+        if(!inRange(nx, ny)||arr[nx][ny]!=0){
+            now = (now+1) %4;
+        }
+        x+=dx[now];
+        y+=dy[now];
+        arr[x][y]=i;
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
