69f7873417f1f2d8ad6564820d96eda6aa9cee0a
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-01-25 12:20:52 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
+++ 240125/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 using namespace std;
-int n,m;
+
+int n, m;
+
 bool inRange(int x, int y) {
     return 0 <= x && x < n && 0 <= y && y < m;
-};
+}
+
 int main() {
-    cin>>n>>m;
-    int arr[101][101]={};
-    int dx[4]={1,0,-1,0};
-    int dy[4]={0,1,0,-1};
-    int x=0,y=0, now=0;
-    arr[x][y]=1;
-    
-    for(int i=1;i<=n*m;){
-        int nx= x+dx[now];
-        int ny= y+dy[now];
-        if(!inRange(nx, ny)||arr[nx][ny]!=0){
-            now = (now+1) %4;
+    cin >> n >> m;
+    int arr[101][101] = {};
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+    int x = 0, y = 0, now = 0;
+    arr[x][y] = 1;
+
+    for (int i = 2; i <= n * m;) {
+        int nx = x + dx[now];
+        int ny = y + dy[now];
+        if (!inRange(nx, ny) || arr[nx][ny] != 0) {
+            now = (now + 1) % 4;
         }
-        i++;
-        x+=dx[now];
-        y+=dy[now];
-        arr[x][y]=i;
+        x += dx[now];
+        y += dy[now];
+        arr[x][y] = i++;
     }
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
-            cout<<arr[i][j]<<" ";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 
     return 0;
