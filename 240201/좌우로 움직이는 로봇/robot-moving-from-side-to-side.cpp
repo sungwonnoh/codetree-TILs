f1ec9887b146a5dca06dc8a96627fbba90dffe45
엔오 <129041262+sungwonnoh@users.noreply.github.com>
2024-02-01 18:04:40 +0900

    Title: 좌우로 움직이는 로봇, Time: 46ms, Memory: 7MB, Status: Passed - CodeTree

--- 240201/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 240201/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-int a[10000]={};
-int b[10000]={};
+int a[1000000]={};
+int b[1000000]={};
 int main() {
     int n,m;
     int cnt=0;
@@ -10,27 +10,38 @@ int main() {
     char d;
     for(int i=0;i<n;i++){
         cin>>t>>d;
-        while(t--){
+        for(int j=0;j<t;j++){
             if(d=='L'){
-                a[++ta]=a[ta-1]-1;
+                a[ta]=a[ta-1]-1;
             }else{
-                a[++ta]=a[ta-1]+1;
+                a[ta]=a[ta-1]+1;
             }
+            ta++;
         }
     }
     for(int i=0;i<m;i++){
         cin>>t>>d;
-        while(t--){
+        for(int j=0;j<t;j++){
             if(d=='L'){
-                b[++tb]=b[tb-1]-1;
+                b[tb]=b[tb-1]-1;
             }else{
-                b[++tb]=b[tb-1]+1;
+                b[tb]=b[tb-1]+1;
             }
+            tb++;
         }
     }
-    int time=min(ta,tb);
-    for(int i=1;i<=time;i++){
-        if(a[i]==b[i]){
+    if(ta<tb){
+        for(int i=ta;i<tb;i++){
+            a[i]=a[i-1];
+        }
+    }else if(ta>tb){
+        for(int i=tb;i<ta;i++){
+            b[i]=b[i-1];
+        }
+    }
+    int time=max(ta,tb);
+    for(int i=1;i<time;i++){
+        if(a[i]==b[i]&&a[i-1]!=b[i-1]){
             cnt++;
         }
     }
